0bf6b5d6e2352493e087792fc046b47b6e36da6d
zeronek <zerone-@naver.com>
2022-03-10 21:30:48 +0900

    plz

---
+++ 02/app02.js
@@ -0,0 +1,55 @@
+var a = 'fe';
+// a = 111;
+console.log(a);
+
+
+var isMan = 'true';
+var isMan1 = true;
+console.log(isMan);
+console.log(isMan1);
+
+
+
+function log(str){
+    console.log(str);
+
+}
+log('hello');
+
+var img={};
+img.width =100;
+img.height = 200;
+console.log(img);
+
+
+var arr = [1,2,3,4,5];
+console.log(arr);
+
+console.log(1 + 1);
+console.log(a + 'hello');
+
+
+var me = {
+    age: 11,
+    a : 'fe',
+    isMan: true,
+    sayHellow : function (){
+        console.log ('hello');
+    },
+};
+me. sayHellow();
+
+
+var arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
+
+for(var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+
+var todos = ['운동'];
+var todo = '게임';
+todos.push(todo);
+
+console.log(todos);
