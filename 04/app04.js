dff31062474593113d0367709ef4c66ea24e73f3
zeronek <zerone-@naver.com>
2022-03-25 16:02:40 +0900

    04

---
+++ 04/app04.js
@@ -0,0 +1,44 @@
+console.log('04');
+// const element1 = document.getElementById('wrap');
+// const element = document.querySelector('.item');
+
+
+// console.log(elements)
+
+// const div = document.createElement('div');
+// div.style.border = "1px solid blue";
+// div.innerHTML = "hello!!";
+
+// document.body.appendChild(div);
+
+
+// document.querySelector('#wrap').innerHTML = 1111;
+
+// window.onload=function(){
+//     console.log ("load");
+// };
+
+
+// var clickCount = 0;
+// function popup(event){
+//     console.log(++clickCount);
+//     console.log(arguments, this ,event ,event.type,event.currentTarget);
+// }
+// document.body.addEventListener('click', popup);
+
+// document.body.addEventListener('click', function(){
+//     console.log("click");
+// });
+
+// var app = document.getElementById("list");
+
+// app.addEventListener("click", function(event) {
+//     if(event.target.nodeName == "LI"){
+//     }   
+//     });
+
+// var link = document.querySelector('a');
+// link.addEventListener('click',funtion()){
+//     window.open('http://www.daum.net');
+//     event.preventDefault();
+
